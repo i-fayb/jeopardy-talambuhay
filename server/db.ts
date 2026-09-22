d9a4dfbe851d62aa15941547024599212b9deadc
Iva <ivette_faybishenko@horacemann.org>
2026-09-22 17:51:36 -0400

    first commit

--- server/db.ts
+++ server/db.ts
@@ -6,83 +6,96 @@ const sortQuestions = (questions: { points: number; question: string; answer: st
 const pastQuestions: Question[] = sortQuestions([
     {
         points: 100,
-        question: 'What US state is the Land of Lincoln?',
-        answer: 'Illinois',
+        question: 'How many years have I been a student at Horace Mann?',
+        answer: '12',
     },
     {
         points: 200,
         question:
-            'Which country\'s flag is this?',
-        imgSrc: "https://cdn.britannica.com/34/4034-050-91EE1BCF/Flag-Myanmar.jpg",
-        answer: 'Myanmar',
+            'Where is the city of Toldeo located?',
+        imgSrc: "static/toledo.png",
+        answer: 'Spain',
     },
     {
         points: 300,
         question:
-            'What Ivy League school has the highest Native American enrollment (a whoppping 1%)?',
-        answer: 'Dartmouth',
+            'An estimated 5% to 9% of people will have what organ removed after it becomes inflamed?',
+        answer: 'appendix',
     },
     {
         points: 400,
-        question: 'Who wrote the Critique of Pure Reason?',
-        answer: 'Immanuel Kant',
+        question: 'Which book character is imprisoned in the Château dIf for 14 years?',
+        answer: 'The Count of Monte Cristo',
     }
 ]);
 
 const presentQuestions: Question[] =
     sortQuestions([
         {
-            points: 400,
-            question:
-                'This is Donu, a character from which video game?',
-            imgSrc: '/donu-gif.gif',
-            answer: 'Slay the Spire',
-        },
-        {
             points: 100,
             question:
-                'Tahini is made from which seed?',
-            imgSrc: 'https://www.aforkstale.com/wp-content/uploads/how-to-make-homemade-tahini-1200-x-1200.jpg',
-            answer: 'Sesame',
+                'Which famous French king is known as the Sun King?',
+                imgSrc: 'static/king.webp',
+            answer: 'Louis XIV',
         },
         {
             points: 200,
-            question: 'What programming language is the below code?',
-            imgSrc: '/programming_language.png',
-            answer: 'Javascript',
+            question:
+                'What US state was the skateboard invented in ?',
+            answer: 'California',
         },
         {
             points: 300,
+            question: 'What is the name of this alphabet?',
+            imgSrc: 'static/cyrillic.png',
+            answer: 'Cyrillic',
+        },
+        {
+            points: 400,
             question:
-                'This country is home to the Dolomites, which are a mountain range that has historical \'via ferratas\', iron cables and rungs, to aid traversing the peaks?',
-            imgSrc:
-                "https://laguidalpina.it/cdn/shop/products/ferrata-marmolada-cresta-ovest-Cristiano-Gregnanin-Guida-Alpina-Certificata-Dolomiti-5.jpg?v=1738870778",
-            answer: 'Italy',
+                'The rebellion against against classical ballet led to which dance style?',
+            answer: 'contemporary',
         }
     ]);
 const futureQuestions: Question[] = sortQuestions([
     {
         points: 100,
         question:
-            'This type of 2D drawing allows you to see the sides of a 3D object at the same scale.',
-        imgSrc:
-            "https://static.mathigon.org/cms/a8141a111490d026fa6578a4933d1d47.png",
-        answer: 'Isometric',
+            'WHo is the author of the book "1984"?',
+        answer: 'George Orwell',
+    },
+    {
+        points: 200,
+        question:
+            'A kip-up is a famous trick in which dance style?',
+        answer: 'hip-hop',
+    },
+    {
+        points: 300,
+        question:
+            'What is the capital of Illinois?',
+        answer: 'Springfield',
+    },
+    {
+        points: 400,
+        question:
+            'Which school in the Ivy Preparatory School League\'s mascot is a hornet?',
+        answer: 'Hackley',
     }
 ]);
 
 
 const categories = [
     {
-        title: 'Ms Feng\'s Past',
+        title: 'Iva\'s Past',
         questions: pastQuestions
     },
     {
-        title: `Ms. Feng's Present`,
+        title: `Iva's Present`,
         questions: presentQuestions
     },
     {
-        title: "Ms. Feng's Future",
+        title: "Iva's Future",
         questions: futureQuestions
     }
 ];
