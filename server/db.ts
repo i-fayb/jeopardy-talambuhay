fcd0ef88872a538f8076337c1620df6c02024a0d
Iva <ivette_faybishenko@horacemann.org>
2026-09-24 13:54:58 -0400

    with comments

--- server/db.ts
+++ server/db.ts
@@ -7,25 +7,25 @@ const pastQuestions: Question[] = sortQuestions([
     {
         points: 100,
         question: 'How many years have I been a student at Horace Mann?',
-        answer: '12',
+        answer: '12', 
     },
     {
         points: 200,
         question:
             'Which country is the city of Toldeo located?',
         imgSrc: "static/toledo.png",
-        answer: 'Spain',
+        answer: 'Spain', //visited last spring break
     },
     {
         points: 300,
         question:
             'An estimated 5% to 9% of people will have what organ removed after it becomes inflamed?',
-        answer: 'appendix',
+        answer: 'appendix', //had appendicitis
     },
     {
         points: 400,
         question: 'Which book character is imprisoned in the Château d\'If for 14 years?',
-        answer: 'The Count of Monte Cristo',
+        answer: 'The Count of Monte Cristo', //favorite book i read this summer
     }
 ]);
 
@@ -36,25 +36,25 @@ const presentQuestions: Question[] =
             question:
                 'Which French king is famously known as the Sun King?',
                 imgSrc: 'static/king.webp',
-            answer: 'Louis XIV',
+            answer: 'Louis XIV', //one of my favorite history subjects
         },
         {
             points: 200,
             question:
                 'Which US state was the skateboard invented in?',
-            answer: 'California',
+            answer: 'California', //dont skateboard but visit california a lot for family
         },
         {
             points: 300,
             question: 'What is the name of this alphabet?',
             imgSrc: 'static/cyrillic.png',
-            answer: 'Cyrillic',
+            answer: 'Cyrillic', //speak russian
         },
         {
             points: 400,
             question:
                 'The rebellion against against ballet led to which dance style?',
-            answer: 'contemporary',
+            answer: 'contemporary', //i dance this style
         }
     ]);
 const futureQuestions: Question[] = sortQuestions([
@@ -62,25 +62,25 @@ const futureQuestions: Question[] = sortQuestions([
         points: 100,
         question:
             'Who is the author of the book "1984"?',
-        answer: 'George Orwell',
+        answer: 'George Orwell', //book i am trying to get my brother to read
     },
     {
         points: 200,
         question:
             'A kip-up is a famous trick in which dance style?',
-        answer: 'hip-hop',
+        answer: 'hip-hop', //am learning this trick
     },
     {
         points: 300,
         question:
             'What is the capital of Illinois?',
-        answer: 'Springfield',
+        answer: 'Springfield', //sister is in chicago for college 
     },
     {
         points: 400,
         question:
             'Which school in the Ivy Preparatory School League\'s mascot is a hornet?',
-        answer: 'Hackley',
+        answer: 'Hackley',//best friend goes to hackley and i plan to visit her soon
     }
 ]);
 
